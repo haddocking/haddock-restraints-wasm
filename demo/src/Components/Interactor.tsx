420972956577eeea0e6225967ff2a24be16a51f6
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-06-17 12:17:28 +0200

    add proof-of-concept

--- demo/src/Components/Interactor.tsx
+++ demo/src/Components/Interactor.tsx
@@ -1,22 +1,28 @@
-import init, { WasmInteractor } from "haddock-restraints-wasm";
+import init, { WasmAir, WasmInteractor } from "haddock-restraints-wasm";
 import { useEffect, useState } from "react";
 
 export const Interactor = () => {
-  const [interactor, setInteractor] = useState<WasmInteractor>();
+  const [executed, setExecuted] = useState<boolean>(false);
 
   async function load() {
     await init();
     const active = new Int16Array([1]);
     const passive = new Int16Array([10]);
-    const api = new WasmInteractor(1, "A", active, passive);
-    setInteractor(api);
+    const interactor_1 = new WasmInteractor(1, "A", active, passive);
+    const interactor_2 = new WasmInteractor(2, "B", active, passive);
+    interactor_1.set_target(interactor_2.id());
+    interactor_2.set_target(interactor_1.id());
+
+    const air = new WasmAir([interactor_1, interactor_2]);
+
+    const result = air.gen_tbl();
+    console.log(result);
+    setExecuted(true);
   }
 
   useEffect(() => {
     load();
-  });
-
-  console.log(interactor?.print());
+  }, [executed]);
 
   return <></>;
 };
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,4 @@
-use haddock_restraints::Interactor;
+use haddock_restraints::{Air, Interactor};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -20,5 +20,37 @@ impl WasmInteractor {
 
     pub fn print(&self) -> String {
         "Hello!".to_string()
+    }
+
+    pub fn set_target(&mut self, target: u16) {
+        self.inner.add_target(target)
+    }
+
+    pub fn id(&self) -> u16 {
+        self.inner.id()
+    }
+}
+
+#[wasm_bindgen]
+pub struct WasmAir {
+    inner: Air,
+}
+
+#[wasm_bindgen]
+impl WasmAir {
+    #[wasm_bindgen(constructor)]
+    pub fn new(interactors: Vec<WasmInteractor>) -> Self {
+        // TODO: Convert WasmInteractor back to Interactor
+        //  use the `inner`
+        WasmAir {
+            inner: Air::new(interactors.into_iter().map(|c| c.inner).collect()),
+        }
+    }
+
+    pub fn gen_tbl(&self) -> String {
+        match self.inner.gen_tbl() {
+            Ok(r) => r,
+            Err(r) => r.to_string(),
+        }
     }
 }
