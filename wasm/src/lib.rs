14e68ac7e22acda6391ed0c8da1638471a7247ff
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-06-26 12:37:24 +0200

    add more wrapping

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -18,16 +18,52 @@ impl WasmInteractor {
         WasmInteractor { inner: interactor }
     }
 
-    pub fn print(&self) -> String {
-        "Hello!".to_string()
-    }
-
     pub fn set_target(&mut self, target: u16) {
         self.inner.add_target(target)
     }
 
     pub fn id(&self) -> u16 {
         self.inner.id()
+    }
+
+    pub fn set_active(&mut self, active: Vec<i16>) {
+        self.inner.set_active(active);
+    }
+
+    pub fn set_active_atoms(&mut self, atoms: Vec<String>) {
+        self.inner.set_active_atoms(atoms);
+    }
+
+    pub fn set_chain(&mut self, chain: &str) {
+        self.inner.set_chain(chain);
+    }
+
+    pub fn set_lower_margin(&mut self, margin: f64) {
+        self.inner.set_lower_margin(margin)
+    }
+
+    pub fn set_passive(&mut self, passive: Vec<i16>) {
+        self.inner.set_passive(passive);
+    }
+
+    pub fn set_structure(&mut self, structure: &str) {
+        todo!()
+    }
+
+    pub fn set_surface_as_passive(&mut self) {
+        self.inner.set_surface_as_passive();
+    }
+
+    pub fn set_target_distance(&mut self, distance: f64) {
+        self.inner.set_target_distance(distance);
+    }
+
+    pub fn set_upper_margin(&mut self, margin: f64) {
+        self.inner.set_upper_margin(margin);
+    }
+
+    pub fn set_passive_atoms(&mut self, atoms: Vec<String>) {
+        self.inner.set_passive_atoms(atoms);
     }
 }
 
@@ -40,8 +76,16 @@ pub struct WasmAir {
 impl WasmAir {
     #[wasm_bindgen(constructor)]
     pub fn new(interactors: Vec<WasmInteractor>) -> Self {
-        // TODO: Convert WasmInteractor back to Interactor
-        //  use the `inner`
+        // NOTE: This needs to wrap around the `read_json_file` function
+        //  And should get the interactors directly
+        // <https://github.com/haddocking/haddock-restraints/blob/362fdf04cf527d89aad0946bbd475c7b17ca6ab6/src/core/commands/tbl.rs#L23-L51>
+        //
+        // TODO: Loop over the interactors and check if:
+        // - need to `set_structure`?
+        // - need to `set_passive_from_active`?
+        // - need to `set_surface_as_passive`?
+        // - need to `remove_buried_residues`?
+        //
         WasmAir {
             inner: Air::new(interactors.into_iter().map(|c| c.inner).collect()),
         }
